07f7a64c9c99afae0df04eef904c9cc67e572efc
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-07-13 21:45:19 +0100

    Creating index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './style.css';
+
+const form = document.querySelector('.form');
+const list = document.querySelector('.mainList');
+const holder = document.querySelector('.add');
+
+const arr = [];
+let toDoId = 0;
+
+function addToList(iteams) {
+  const item = document.createElement('div');
+  item.classList.add('item');
+  item.classList.add(toDoId);
+  item.innerHTML += `
+    <input type="checkbox" name="" id="${toDoId}">
+    <p>${holder.value}</p>
+  `
+  list.appendChild(item);
+}
+
+function createIteam(text) {
+  const iteams = {
+    description: text,
+    completed: false,
+    index: toDoId += 1,
+  }
+  arr.push(iteams);
+  addToList(iteams);
+  console.log(iteams);
+  holder.value = '';
+}
+
+  form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  createIteam(holder.value);
+});
